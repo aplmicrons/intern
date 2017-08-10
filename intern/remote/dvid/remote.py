4c13cf1ee82cf78eab7a24a63c71be4db32505d3
Luismiguel Rodriguez <Luismiguel Rodriguez>
2017-08-10 11:33:59 -0400

    Adds log, info and other functionalities to the dvid remote

--- intern/remote/dvid/remote.py
+++ intern/remote/dvid/remote.py
@@ -118,5 +118,5 @@ class DVIDRemote(Remote):
 		#Allows the user to write a short description of the content in the repository
 		#{ "log": [ "provenance data...", "provenance data...", ...] }
 		log = requests.post("http://34.200.231.1/api/node/" + UUID + "/log",
-			data = {"log" : [log1] })
+			body = {"log" : [log1] })
 		return("The log has been updated.")
