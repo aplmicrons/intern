07b2a32b581a505644f4ef2d2a05aaae76de7fd8
Rodriguez <rodrilm2@itsd-280559-ml.jhuapl.edu>
2017-09-22 17:33:49 -0400

    Fixes Init mistake

--- intern/remote/__init__.py
+++ intern/remote/__init__.py
@@ -15,12 +15,9 @@
 """
 Remotes are services that are accessed via HTTP/SCP or other services that
 talk to non-local machines.
-
-Author: 
-	Luis Rodriguez
 """
 
+# from __future__ import absolute_import
 
-
-from intern.remote.dvid.remote import DVIDRemote
+from intern.remote.remote import Remote
 	
