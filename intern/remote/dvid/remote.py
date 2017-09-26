b62b4e11a6a83fc78c92c8ece2c4ea8b6d6980f2
Your NameLuis <you@example.comlrodri729@gmail.com>
2017-09-26 14:51:55 -0400

    Adds create_project UUID Detection

--- intern/remote/dvid/remote.py
+++ intern/remote/dvid/remote.py
@@ -89,7 +89,7 @@ class DVIDRemote(Remote):
 	    #User entered IP address with added octet-stream line to obtain data from api in octet-stream form
 	    #0_1_2 specifies a 3 dimensional octet-stream "xy" "xz" "yz"
 	    
-	    address = api + "api/node/" + ID + "/" + repos + "/raw" + "/0_1_2/" + size + "/" + offset + "/octet-stream" 
+	    address = api + "/api/node/" + ID + "/" + repos + "/raw" + "/0_1_2/" + size + "/" + offset + "/octet-stream" 
 	    r = requests.get(address)
 	    octet_stream = r.content
 
