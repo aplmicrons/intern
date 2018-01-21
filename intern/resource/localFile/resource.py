0bfe56ac0025aa5bfc82ca329317e0bd1207b1d6
lrodri729@gmail.com <lrodri729@gmail.com>
2018-01-21 18:09:21 -0500

    get_channel and get_cutout added

--- intern/resource/localFile/resource.py
+++ intern/resource/localFile/resource.py
@@ -105,8 +105,6 @@ class LocalResource(Resource):
         zpix = zspan[1]-zspan[0]
         zo = zspan[0]
 
-        print datastore
-        print path
         dataLoc = datastore[path]
         vol = dataLoc[zo:zpix,yo:ypix,xo:xpix]
         return vol
